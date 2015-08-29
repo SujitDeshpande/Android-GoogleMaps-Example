791374efb94a1291a58a201577b40c2a92b2e6db
SujitDeshpande <sujit.md@gmail.com>
2015-08-29 18:26:59 +0530

    Latest Code

--- app/src/main/java/com/samwaad/meramaps/MapsActivity.java
+++ app/src/main/java/com/samwaad/meramaps/MapsActivity.java
@@ -1,15 +1,15 @@
 package com.samwaad.meramaps;
 
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
-import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
@@ -17,23 +17,54 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.text.DateFormat;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
-public class MapsActivity extends FragmentActivity implements
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
+public class MapsActivity
+        extends FragmentActivity
+        implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
     private static final String TAG = "LocationActivity";
-    private static final long INTERVAL = 1000 * 5 * 1; //1 minute
+    private static final long INTERVAL = 1000 * 60 * 1; //1 minute
     private static final long FASTEST_INTERVAL = 1000 * 5 * 1; // 1 minute
 
-    LocationRequest mLocationRequest;
-    GoogleApiClient mGoogleApiClient;
-    Location mCurrentLocation;
-    String mLastUpdateTime;
-    GoogleMap googleMap;
+    private LocationRequest mLocationRequest;
+    private GoogleApiClient mGoogleApiClient;
+    private Location mCurrentLocation;
+    private String mLastUpdateTime;
+    private GoogleMap googleMap;
+    protected LocationManager locationManager;
+    private android.location.LocationListener locationListener;
+    private Marker mMarker;
+    List<Markers> markers = new ArrayList<Markers>();
+
+
+    // flag for GPS status
+    boolean isGPSEnabled = false;
+
+    // flag for network status
+    boolean isNetworkEnabled = false;
+
+    // flag for GPS status
+    boolean canGetLocation = false;
+
+    //Location location; // location
+    double latitude; // latitude
+    double longitude; // longitude
+
+    // The minimum distance to change Updates in meters
+    private static final long MIN_DISTANCE_CHANGE_FOR_UPDATES = 10; // 10 meters
+
+    // The minimum time between updates in milliseconds
+    private static final long MIN_TIME_BW_UPDATES = 1000 * 5 * 1; // 1 minute
+
 
     protected void createLocationRequest() {
         mLocationRequest = new LocationRequest();
@@ -42,9 +73,24 @@ public class MapsActivity extends FragmentActivity implements
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
     }
 
+    public static List<Markers> getData (){
+        List<Markers> data = new ArrayList<>();
+        double[] lats = {12.9715011, 12.968064};
+        double[] longs = {77.5959849, 77.5950677};
+        for (int i=0; (i < lats.length); i++){
+            Markers current = new Markers();
+            current.latitude = lats [i];
+            current.longitude = longs [i];
+            data.add(current);
+        }
+        return data;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(android.view.WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        markers = getData();
         //setContentView(R.layout.activity_maps);
         Log.d(TAG, "onCreate ...............................");
         //show error dialog if GoolglePlayServices not available
@@ -67,7 +113,7 @@ public class MapsActivity extends FragmentActivity implements
         googleMap.setOnMyLocationButtonClickListener(new GoogleMap.OnMyLocationButtonClickListener() {
             @Override
             public boolean onMyLocationButtonClick() {
-                mCurrentLocation = googleMap.getMyLocation();
+                getLocation();
                 addMarker();
                 return true;
             }
@@ -86,7 +132,7 @@ public class MapsActivity extends FragmentActivity implements
         super.onStop();
         Log.d(TAG, "onStop fired ..............");
         mGoogleApiClient.disconnect();
-        Log.d(TAG, "isConnected ...............: " + mGoogleApiClient.isConnected());
+        Log.d(TAG, "isConnected ...............: ");
     }
 
     private boolean isGooglePlayServicesAvailable() {
@@ -101,13 +147,14 @@ public class MapsActivity extends FragmentActivity implements
 
     @Override
     public void onConnected(Bundle bundle) {
-        Log.d(TAG, "onConnected - isConnected ...............: " + mGoogleApiClient.isConnected());
+        Log.d(TAG, "onConnected - isConnected ...............: ");
         startLocationUpdates();
     }
 
     protected void startLocationUpdates() {
-        PendingResult<Status> pendingResult = LocationServices.FusedLocationApi.requestLocationUpdates(
-                mGoogleApiClient, mLocationRequest, this);
+/*        PendingResult<Status> pendingResult = LocationServices.FusedLocationApi.requestLocationUpdates(
+                mGoogleApiClient, mLocationRequest, this);*/
+        getLocation();
         Log.d(TAG, "Location update started ..............: ");
     }
 
@@ -122,6 +169,17 @@ public class MapsActivity extends FragmentActivity implements
         mCurrentLocation = location;
         mLastUpdateTime = DateFormat.getTimeInstance().format(new Date());
         addMarker();
+    }
+    private void addMarkers() {
+        googleMap.clear();
+        for (int i = 0; i < markers.size(); i++) {
+            Markers currentMarker = markers.get(i);
+
+            MarkerOptions markerOptions = new MarkerOptions()
+                    .position(new LatLng(currentMarker.latitude, currentMarker.longitude));
+
+            googleMap.addMarker(markerOptions);
+        }
     }
 
     private void addMarker() {
@@ -138,14 +196,13 @@ public class MapsActivity extends FragmentActivity implements
 
         LatLng currentLatLng = new LatLng(mCurrentLocation.getLatitude(), mCurrentLocation.getLongitude());
         options.position(currentLatLng);
-        //Marker mapMarker = googleMap.addMarker(options);
-        long atTime = mCurrentLocation.getTime();
-        mLastUpdateTime = DateFormat.getTimeInstance().format(new Date(atTime));
-        //mapMarker.setTitle(mLastUpdateTime);
-        //Log.d(TAG, "Marker added.............................");
+
+        //mMarker = googleMap.addMarker(options);
+        Log.d(TAG, "Marker added.............................");
         googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng,
                 17));
         Log.d(TAG, "Zoom done.............................");
+        addMarkers();
     }
 
     @Override
@@ -160,9 +217,10 @@ public class MapsActivity extends FragmentActivity implements
     }
 
     protected void stopLocationUpdates() {
-        LocationServices.FusedLocationApi.removeLocationUpdates(
-                mGoogleApiClient, this);
+/*        LocationServices.FusedLocationApi.removeLocationUpdates(
+                mGoogleApiClient, this);*/
         Log.d(TAG, "Location update stopped .......................");
+        locationManager.removeUpdates(locationListener);
     }
 
     @Override
@@ -172,5 +230,103 @@ public class MapsActivity extends FragmentActivity implements
             startLocationUpdates();
             Log.d(TAG, "Location update resumed .....................");
         }
+    }
+
+    public void getLocation() {
+        try {
+            locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+            locationListener = new android.location.LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    mCurrentLocation = location;
+                }
+
+                @Override
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+
+                }
+
+                @Override
+                public void onProviderEnabled(String provider) {
+
+                }
+
+                @Override
+                public void onProviderDisabled(String provider) {
+
+                }
+            };
+            // getting GPS status
+            isGPSEnabled = locationManager
+                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
+            // getting network status
+            isNetworkEnabled = locationManager
+                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+
+            if (!isGPSEnabled && !isNetworkEnabled) {
+                // no network provider is enabled
+            } else {
+                this.canGetLocation = true;
+
+                // if GPS Enabled get lat/long using GPS Services
+                if (isGPSEnabled) {
+                    if (mCurrentLocation == null) {
+                        locationManager.requestLocationUpdates(
+                                LocationManager.GPS_PROVIDER,
+                                MIN_TIME_BW_UPDATES,
+                                MIN_DISTANCE_CHANGE_FOR_UPDATES, locationListener);
+                        Log.d("GPS Enabled", "GPS Enabled");
+                        if (locationManager != null) {
+                            mCurrentLocation = locationManager
+                                    .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+                            if (mCurrentLocation != null) {
+                                latitude = mCurrentLocation.getLatitude();
+                                longitude = mCurrentLocation.getLongitude();
+                            }
+                            Toast.makeText(
+                                    getApplicationContext(),
+                                    "GPS Location is - \nLat: " + latitude
+                                            + "\nLong: " + longitude,
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                } else {
+                    Toast.makeText(
+                            getApplicationContext(),
+                            "GPS Not Enabled",
+                            Toast.LENGTH_LONG).show();
+                    if (isNetworkEnabled) {
+
+                        locationManager.requestLocationUpdates(
+                                LocationManager.NETWORK_PROVIDER,
+                                MIN_TIME_BW_UPDATES,
+                                MIN_DISTANCE_CHANGE_FOR_UPDATES, locationListener);
+                        Log.d("Network", "Network");
+                        if (locationManager != null) {
+                            mCurrentLocation = locationManager
+                                    .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+                            if (mCurrentLocation != null) {
+                                latitude = mCurrentLocation.getLatitude();
+                                longitude = mCurrentLocation.getLongitude();
+                            }
+
+                            Toast.makeText(
+                                    getApplicationContext(),
+                                    "Network Location is - \nLat: " + latitude
+                                            + "\nLong: " + longitude,
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                }
+
+                addMarker();
+
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
---
+++ app/src/main/java/com/samwaad/meramaps/Markers.java
@@ -0,0 +1,10 @@
+package com.samwaad.meramaps;
+
+/**
+ * Created by Sujit on 29/08/15.
+ */
+public class Markers {
+    double latitude;
+    double longitude;
+}
+
